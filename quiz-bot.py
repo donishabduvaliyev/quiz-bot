10168ef0974bb74b47e1c35f999f845e1f816909
donishabduvaliyev <donishkratos413677@gmail.com>
2025-05-24 23:40:14 +0500

    add-new-subject-atj

--- quiz-bot.py
+++ quiz-bot.py
@@ -164,7 +164,7 @@ def load_questions(file_path):
 
 # === Helper Function for Start Keyboard ===
 def get_start_keyboard(context: ContextTypes.DEFAULT_TYPE) -> InlineKeyboardMarkup | None:
-    known_subjects = ["osimlik-moyi" , "yogni-qayta-ishlash" , "oziq-ovqat-texnologiyasi"] # These should match keys from tests.txt
+    known_subjects = ["osimlik-moyi" , "yogni-qayta-ishlash" , "oziq-ovqat-texnologiyasi" , "ATJ"] # These should match keys from tests.txt
     keyboard = []
     loaded_subjects = context.bot_data.get('questions', {})
 
